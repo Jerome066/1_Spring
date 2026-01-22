60147dc86b595e2afb1fce492831f4fde7ff9321
Jorge <jor.tec.vill@gmail.com>
2026-01-22 16:12:04 -0600

    JPA con BD mysql con entity

--- practica1/src/main/java/com/spring1basic/practica1/controller/UserController.java
+++ practica1/src/main/java/com/spring1basic/practica1/controller/UserController.java
@@ -41,9 +41,9 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public String show(Model model, @PathVariable("id") Long id) {
-        User user1 = new User("Emma", "emma@gmail.com", 1L);
-        User user2 = new User("Manuel", "manuel@gmail.com", 2L);
-        User user3 = new User("Pame", "pame@gmail.com", 3L);
+        User user1 = new User(1L, "Emma", "emma@gmail.com");
+        User user2 = new User(2L,"Manuel", "manuel@gmail.com");
+        User user3 = new User(3L,"Pame", "pame@gmail.com");
 
         return "users/show";
     }
--- practica1/src/main/java/com/spring1basic/practica1/model/User.java
+++ practica1/src/main/java/com/spring1basic/practica1/model/User.java
@@ -1,6 +1,10 @@
 package com.spring1basic.practica1.model;
 
 import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
-    private Long id;
+    
 
 }
